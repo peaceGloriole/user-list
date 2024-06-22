e11430e7f7135301ed47d287dfc6a5240797f83f
peaceGloriole <dmtrpwlw@icloud.com>
2024-06-22 21:44:49 +0300

    getAll updated

--- client/src/components/UserContainer.jsx
+++ client/src/components/UserContainer.jsx
@@ -1,9 +1,14 @@
 import Search from "./Search";
+import UserList from "./UserList";
 
 const UserContainer = () => {
     return (
         <section className="card users-container">
-            <Search />    
+            <Search /> 
+
+            <UserList />   
+
+            <button className="btn-add btn">Add new user</button>
         </section>
     );
 };
